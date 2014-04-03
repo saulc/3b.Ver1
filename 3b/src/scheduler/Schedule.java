74938a2ebe774a54a2ab1fe1798bf8263566b288
saul <saulc89@gmail.com>
2014-04-03 16:05:54 -0700

    first commit
    just getting things set up

---
+++ 3b/src/scheduler/Schedule.java
@@ -0,0 +1,14 @@
+package scheduler;
+
+public class Schedule {
+
+	/**
+	 * test. come on git hub work!
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
