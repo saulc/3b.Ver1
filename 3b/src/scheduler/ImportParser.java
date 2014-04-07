96cb4a768e1b929b6d2d2e5ac14607c7641dff6f
vtrung <svingt@gmail.com>
2014-04-07 09:08:35 -0700

    Made a csv parser that will take the string values into an ArrayList of Request
    Change-Id: I0938b147c838e99775dc7a2b9dc9dff12d9d2f14

---
+++ 3b/src/scheduler/ImportParser.java
@@ -0,0 +1,47 @@
+package scheduler;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ImportParser {
+	private ArrayList<Request> requests = new ArrayList<Request>(); //Parse csv into this ArrayList
+	
+	ImportParser(){};
+	
+	public void parse(File file, Scanner in){
+		String line;
+		String word;
+		char letter;
+		int arrayCount;
+		String[] request = new String[4]; 
+		
+		
+		while(in.hasNext()){
+			line = in.nextLine();//System.out.println(line);
+			word = "";
+			arrayCount = 0;
+			
+			for(int count = 0; count < line.length(); count++){
+				letter = line.charAt(count);
+				if(letter != ';'){
+					//System.out.print(letter);
+					word += letter;
+				} else {
+					request[arrayCount] = word;
+					arrayCount++;
+					//System.out.print("AR" + arrayCount);
+					word = "";
+				}
+			}
+			
+			Request mRequest = new Request(request[0], 
+						request[1], 
+						request[2], 
+						request[3]);
+			
+			requests.add(mRequest);
+		};
+		in.close();
+	}
+}
