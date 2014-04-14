3d6d6e28a0283f9ad1ed9a78fcefd0e5e2174a77
saul <saulc89@gmail.com>
2014-04-14 15:30:30 -0700

    added table model and modified course class

--- 3b/src/Classes/Course.java
+++ 3b/src/Classes/Course.java
@@ -4,20 +4,33 @@ public class Course {
 	
 	private String Dept;
 	private String Code;
+	private Instructor Prof;
 	
-	public Course()
+	public Course(String department, String code, Instructor prof)
 	{
-		Dept="";
-		Code="";
+		Dept = department;
+		Code = code;
+		if(prof != null)
+			Prof = prof;
 	}
 	
-	public String getDept(String department)
+	public String getDept()
 	{
-		return Dept=department; //return the department, such as Math and CS
+		return Dept; //return the department, such as Math and CS
 	}
 	
-	public String getCode(String course_ID)
+	public String getCode()
 	{
-		return Code=course_ID; //return the course number
+		return Code; //return the course number
+	}
+	public Instructor getInstructor(){
+		return Prof;
+	
+	}
+	//sets new instructor and returns previous one
+	public Instructor swapInstructor(Instructor newProf){
+		Instructor temp = Prof;
+		Prof = newProf;
+		return temp;
 	}
 }
---
+++ 3b/src/scheduler/TableModel.java
@@ -0,0 +1,61 @@
+package scheduler;
+
+import java.util.Vector;
+
+import javax.swing.table.AbstractTableModel;
+
+import Classes.Course;
+
+
+public class TableModel extends AbstractTableModel {
+	private Vector<String> cNames;	
+	private Vector<Object> data;
+	
+	public TableModel( Vector<Object> data){
+		super();
+		this.data = data;
+		loadData();
+	}
+	public void setData(Vector<Object> data){
+		this.data = data;
+	}
+	public void addItem(Course course ){
+		data.add(course);
+	}
+	
+	@Override
+	public String getColumnName(int c){
+		return cNames.get(c);
+	}
+	@Override
+	public int getColumnCount() {
+		
+		return cNames.size();
+	}
+
+	@Override
+	public int getRowCount() {
+		
+		return data.size();
+	}
+
+	@Override
+	public Object getValueAt(int x, int y) {
+		switch(y){
+		case 0 : return (((Course) data.get(x)).getDept());
+		case 1 : return (((Course) data.get(x)).getCode());
+		}
+		return "";
+	}
+	
+
+	private void loadData() {
+		String[] colums = new String[] {
+				"Subject", "CRN", "Days", "Hours", "ID", "Notes"
+			};
+		cNames	 = new Vector<String>(); 
+		for(int i=0; i<colums.length; i++)
+			cNames.add(colums[i]);
+		
+	}
+}
