06136c3bb4aaf16c8152895a53b4023def226b61
vtrung <svingt@gmail.com>
2014-04-16 13:55:30 -0700

    Updated InstructorList

--- 3b/src/list/InstructorList.java
+++ 3b/src/list/InstructorList.java
@@ -9,5 +9,42 @@ public class InstructorList {
 	
 	public InstructorList(){}
 	
+	private boolean checkDuplicate(String ID){
+		for(int index = 0; index < instructors.size(); index++){
+			if(instructors.get(index).getID() == ID){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void generateList(RequestList requests){
+		String name, ID;
+		boolean Adjunct;
+		for(int count = 1; count < requests.size(); count++){ //ignore first line, title line
+			name = requests.getItem(count, 1);
+			ID = requests.getItem(count, 2);
+			if(requests.getItem(count, 3) == "yes"){
+				Adjunct = true;
+			} else {
+				Adjunct = false;
+			}
+			if(!checkDuplicate(ID)){
+				instructors.add(new Instructor(name,ID,Adjunct));
+			}
+		}
+	}
+	
+	public Instructor getInstructor(int index){
+		return instructors.get(index);
+	}
+	
+	public void addInstructor(Instructor instructor){
+		instructors.add(instructor);
+	}
+	
+	public void removeInstructor(int index){
+		instructors.remove(index);
+	}
 	
 }
